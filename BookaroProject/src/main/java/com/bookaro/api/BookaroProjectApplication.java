b8cbce56280a56b5762328d0428ca3ee6bdb3a47
pEDrOxZzz666 <Pedro@Puesto376.infortisa.local>
2022-03-22 16:31:26 +0100

    primer commit

---
+++ BookaroProject/src/main/java/com/bookaro/api/BookaroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bookaro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookaroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookaroProjectApplication.class, args);
+	}
+
+}
---
+++ BookaroProject/src/test/java/com/bookaro/api/BookaroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookaro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
